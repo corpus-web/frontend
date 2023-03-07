15b99046168b063246fa11c51063b784c53aec89
wu fang <wufangcs@hrbeu.edu.cn>
2023-03-07 17:19:45 +0800

    0307

--- src/view/MyManage.vue
+++ src/view/MyManage.vue
@@ -4,8 +4,13 @@
             <div style="position: relative;width: 100%;height: 4rem;">
                 <h2 style="width:80%;position: absolute;font-family:'微软雅黑';font-size: 30px;margin-left:42%;">后台管理系统
                 </h2>
-                <el-button type="info" style="position: absolute;right: 0;top: 1.5rem;" @click="exit()">退出
-                </el-button>
+                <div style="position:absolute;right: 0;top: 1.5rem;">
+                    <el-button type="info" @click="change_pwd()">
+                        修改密码
+                    </el-button>
+                    <el-button type="info" @click="exit()">退出
+                    </el-button>
+                </div>
             </div>
             <el-menu default-active="1" class="el-menu-demo" mode="horizontal" @select="handleSelect"
                 background-color="#545c64" text-color="#fff" active-text-color="#ffd04b"
@@ -18,7 +23,25 @@
                 <el-menu-item index="6">语料库文章</el-menu-item>
             </el-menu>
         </div>
+        <el-dialog title="修改密码" :visible.sync="show_change_pwd" width="500px">
+            <el-form :model="change_pass" label-width="70px" label-position="left">
+                <el-form-item label="新密码">
+                    <el-input v-model="change_pass.new_pass" show-password>
 
+                    </el-input>
+                </el-form-item>
+                <el-form-item label="确认密码">
+                    <el-input v-model="change_pass.qr" show-password>
+
+                    </el-input>
+                </el-form-item>
+            </el-form>
+            <div>
+                <el-button @click="ok_change()" type="success" style="margin-left:186px">
+                    确认修改
+                </el-button>
+            </div>
+        </el-dialog>
         <div id="main" v-show="active == 1">
             <lbt :part="active" :id="1"></lbt>
 
@@ -57,9 +80,47 @@ export default {
         return {
             active: 1,
             MainSwiper: [],
+            show_change_pwd: false,
+            change_pass: {
+                new_pass: "",
+                qr: "",
+            }
         }
     },
     methods: {
+        ok_change() {
+            if (this.change_pass.new_pass == this.change_pass.qr)//相等
+            {
+                this.show_change_pwd = false;
+                this.$axios.request({
+                    method: 'POST',
+                    url: "/api/user/change",
+                    data: {
+                        token: localStorage.getItem('token'),
+                        new_password: this.change_pass.new_pass,
+                    },
+                }).then((res) => {
+                    if (res.status == 200) {
+                        this.$message({
+                            type: 'success',
+                            message: 'ok',
+                        }),
+                            window.localStorage.removeItem('token');//删去token
+                        this.$router.push('/login');//路由跳转
+                    }
+                })
+
+            }
+            else {
+                this.$message({
+                    type: 'error',
+                    message: '确认密码与新密码不匹配',
+                })
+            }
+        },
+        change_pwd() {
+            this.show_change_pwd = true;
+        },
         curs(row) {
             if (row.column.label === "图片地址")
                 return "cursor:pointer;"
