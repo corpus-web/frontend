f299f1ccc5c3f59c27fec5e2f59228ccc54176fc
xhd0728 <xinhaidong0625@foxmail.com>
2023-05-05 11:47:18 +0800

    还原needValid

--- src/globle/globleApi.js
+++ src/globle/globleApi.js
@@ -1,5 +1,5 @@
-// const baseURL = "http://corpus.hrbeu.edu.cn"
-const baseURL = "http://192.168.31.33:8000"
+const baseURL = "http://corpus.hrbeu.edu.cn"
+// const baseURL = "http://192.168.31.200:8000"
 
 export default {
     baseURL
